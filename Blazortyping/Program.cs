2df01abf9a76768397d5a58b237c9c0113e528df
susujeong <pqpqpq0412@gmail.com>
2024-12-20 13:25:48 +0900

    초기 커밋.

---
+++ Blazortyping/Program.cs
@@ -0,0 +1,14 @@
+using Blazortyping;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// 서버 API 호출을 위한 HttpClient 등록
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
+
+await builder.Build().RunAsync();
---
+++ Server/Controllers/CsvController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Server.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class CsvController : ControllerBase
+	{
+		[HttpGet("read")]
+		public IActionResult ReadCsv()
+		{
+			//string filePath = Path.Combine(Directory.GetCurrentDirectory(), "word.csv");
+			string filePath = @"C:\Users\user\Desktop\241203\word.csv";
+
+			if (!System.IO.File.Exists(filePath))
+			{
+				Console.WriteLine("지정된 경로에 파일이 존재하지 않습니다.");
+				return NotFound("CSV 파일이 존재하지 않습니다.");
+			}
+
+			using (var reader = new StreamReader(filePath, Encoding.UTF8))
+			{
+				var content = reader.ReadToEnd();
+				return Ok(content);
+			}
+		}
+	}
+}
